e983100e0cc2cc720e5c239f46560f86432913a4
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2020-03-28 20:04:51 +0100

    Fixed script to compare to server image

---
+++ README.md
@@ -0,0 +1,4 @@
+# Install
+You need Python3+, with libraries numpy, opencv-python and requests.
+
+You can install them with : `pip install numpy opencv-python requests` or `pip3 install numpy opencv-python requests` following your install.
--- pixelwar.py
+++ pixelwar.py
@@ -1,8 +1,16 @@
 import numpy as np
-import cv2
-import random, hashlib, requests
+import cv2, random, hashlib, requests
 
-img = cv2.imread('cec.jpg')
+img = cv2.imread('cec-small.jpg')
+
+def getHex(bgr):
+    return '#%02x%02x%02x' % tuple(reversed(bgr))
+
+def getH25img():
+    url = r'http://pixelwar.h25.io/image'
+    resp = requests.get(url, stream=True).raw
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    return cv2.imdecode(image, cv2.IMREAD_COLOR)
 
 def setpixel(x,y,color, proof):
     params = {'x':str(x),
@@ -12,19 +20,24 @@ def setpixel(x,y,color, proof):
     r = requests.get('http://137.74.47.86/setpixel', params=params)
     return r.text == 'OK'
 
-for x in range(20):
-    for y in range(20):
-        color = '#%02x%02x%02x' % tuple(reversed(img[x,y]))
-        # print(img[x,y])
-        # proof = ''
-        # hashFound = False
-        # while not hashFound:
-        #     proof = ''.join([random.choice('h25io') for _ in range(30)])
-        #     if hashlib.sha256(('h25'+proof).encode()).hexdigest().startswith('00000'):
-        #         hashFound = True
-        # sent = False
-        # while not sent:
-        #     sent = setpixel(x+40,y+40,color[1:],proof)
-        print('OK for pixel x ', x, ' and y ', y, ' with color ', color)
+while True:
+    imgH25 = getH25img()
+    print('Loaded image from H25')
+    for x in range(20):
+        for y in range(30):
+            print('Processing (',x,',',y,')')
+            color = getHex(img[abs(19-x),abs(29-y)])
+            colorH25 = getHex(imgH25[x+20,y+60])
 
-# exemple : setpixel(60,60,'ffffff')
+            if color != colorH25:
+                print('Color is different ! Us ',color,'| Them ',colorH25)
+                proof = ''
+                hashFound = False
+                while not hashFound:
+                    proof = ''.join([random.choice('h25io') for _ in range(30)])
+                    if hashlib.sha256(('h25'+proof).encode()).hexdigest().startswith('00000'):
+                        hashFound = True
+                sent = False
+                while not sent:
+                    sent = setpixel(x+40,y+40,color[1:],proof)
+                print('Corrected')
