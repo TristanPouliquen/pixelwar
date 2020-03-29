6cc35e3b8b5896db7cd2da5f9cb6f78656bc3a72
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2020-03-29 14:08:33 +0200

    pixelwar random

--- pixelwar.py
+++ pixelwar.py
@@ -1,7 +1,5 @@
 import numpy as np
-import cv2, random, hashlib, requests
-
-img = cv2.imread('cec-small.jpg')
+import cv2, random, hashlib, requests, string
 
 def getHex(bgr):
     return '#%02x%02x%02x' % tuple(reversed(bgr))
@@ -18,28 +16,40 @@ def setpixel(x,y,color, proof):
                 'color':color,
                 'proof':proof}
     r = requests.get('http://137.74.47.86/setpixel', params=params)
-    return r.text == 'OK'
+    return r.text
 
+def getPixelMap():
+    img = cv2.imread('cec-small.jpg')
+    [x,y,_] = img.shape
+    pixels = []
+    for i in range(x):
+        for j in range(y):
+            pixels.append([i,j,getHex(img[i,j])])
+    return pixels
+
+def getProof():
+    proof = ''
+    hashFound = False
+    while not hashFound:
+        proof = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+        if hashlib.sha256(('h25'+proof).encode()).hexdigest().startswith('00000'):
+            hashFound = True
+    return proof
+
+pixels = getPixelMap()
 while True:
     imgH25 = getH25img()
     print('Loaded image from H25')
-    for x in range(20):
-        for y in range(30):
-            print('Processing (',x,',',y,')')
-            i = x+20
-            j = y+60
-            color = getHex(img[abs(19-x),abs(29-y)])
-            colorH25 = getHex(imgH25[i,j])
+    [x,y,color] = random.choice(pixels)
+    while color == getHex(imgH25[x+20,y+60]):
+        [x,y,color] = random.choice(pixels)
+    print('Processing (',x,',',y,')')
+    i = x+20
+    j = y+60
+    colorH25 = getHex(imgH25[i,j])
 
-            if color != colorH25:
-                print('Color is different ! Us ',color,'| Them ',colorH25)
-                proof = ''
-                hashFound = False
-                while not hashFound:
-                    proof = ''.join([random.choice('h25io') for _ in range(30)])
-                    if hashlib.sha256(('h25'+proof).encode()).hexdigest().startswith('00000'):
-                        hashFound = True
-                sent = False
-                while not sent:
-                    sent = setpixel(j,i,color[1:],proof)
-                print('Corrected')
+    if color != colorH25:
+        print('Color is different ! Us ',color,'| Them ',colorH25)
+        proof = getProof()
+        reqResult = setpixel(j,i,color[1:],proof)
+        print('Request result: ', reqResult)
